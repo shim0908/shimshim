50926584805c117b61e7582efa1b5a6cdfd47011
shim0908 <shim0908@gmail.com>
2024-06-11 21:31:35 +0900

    Create 0611.py

--- 0611.py
+++ 0611.py
@@ -3,4 +3,4 @@ import collections
 
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
-    return list(answer.keys())[0]
+    return list(answer.keys())
