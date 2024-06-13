267c06e58496ecf3b4beb39c22df9409805967de
shim0908 <shim0908@gmail.com>
2024-06-13 21:38:32 +0900

    Create 0613.py

---
+++ 0613.py
@@ -0,0 +1,6 @@
+def solution(clothes):
+    from collections import Counter
+    from functools import reduce
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
