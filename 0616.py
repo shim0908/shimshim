5af7de31932013057423270a793368903a658f91
shim0908 <shim0908@gmail.com>
2024-06-16 20:02:59 +0900

    Create 0616.py

---
+++ 0616.py
@@ -0,0 +1,8 @@
+def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
