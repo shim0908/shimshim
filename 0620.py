98cec2b85546bb5776efe288916fe4d6c064666f
shim0908 <shim0908@gmail.com>
2024-06-20 21:22:42 +0900

    Create 0620.py

---
+++ 0620.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    stack = []
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        if stack != []:
+            while stack != [] and stack[-1][1] > prices[i]:
+                past, _ = stack.pop()
+                answer[past] = i - past
+        stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
+    return answer
