954d66acb573c8153971ee71e5c53507aef1c305
shim0908 <shim0908@gmail.com>
2024-06-21 21:58:12 +0900

    Create 0621.py

---
+++ 0621.py
@@ -0,0 +1,17 @@
+import heapq as hq
+
+def solution(scoville, K):
+
+    hq.heapify(scoville)
+    answer = 0
+    while True:
+        first = hq.heappop(scoville)
+        if first >= K:
+            break
+        if len(scoville) == 0:
+            return -1
+        second = hq.heappop(scoville)
+        hq.heappush(scoville, first + second*2)
+        answer += 1  
+
+    return answer
