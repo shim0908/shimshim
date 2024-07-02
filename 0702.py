ccdf39de815aec7215d4527d9e87c8c9e38c38b4
shim0908 <shim0908@gmail.com>
2024-07-02 21:17:58 +0900

    Cre

---
+++ 0702.py
@@ -0,0 +1,7 @@
+def solution(n, wires):
+    ans = n
+    for sub in (wires[i+1:] + wires[:i] for i in range(len(wires))):
+        s = set(sub[0])
+        [s.update(v) for _ in sub for v in sub if set(v) & s]
+        ans = min(ans, abs(2 * len(s) - n))
+    return ans
